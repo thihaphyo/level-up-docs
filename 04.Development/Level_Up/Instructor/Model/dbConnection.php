9ecc972f9ad79ba1009f9efe26605460bfb7f16c
LK1130 <67467714+LK1130@users.noreply.github.com>
2022-07-15 20:39:53 +0630

    update

---
+++ 04.Development/Level_Up/Instructor/Model/dbConnection.php
@@ -0,0 +1,19 @@
+<?php
+
+class DBConnect{
+    private $hostname = "localhost";
+    private $port = 3306;
+    private $dbname = "";
+    private $username = "root";
+    private $password = "";
+
+    public function connect(){
+        //connection
+        $pdo = new PDO("mysql:host=$this->hostname;port=$this->port;dbname=$this->dbname",$this->username, $this->password);
+        //set error reply
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $pdo;
+    }
+}
+?>
