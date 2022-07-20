3fde3d9f293656750c90b4ff9161c9f5fa1c9f0e
myot0253@gmail.com <myot0253@gmail.com>
2022-07-19 19:14:30 -1200

    Service and Guide

---
+++ 04.Development/Level_Up/Student/View/Service.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Level Up</title>
+    <link rel="stylesheet" href="./resources/css/mystyles.css">
+    <link rel="stylesheet" href="./resources/css/root.css">
+    <link rel="stylesheet" href="Service.css">
+</head>
+
+<body>
+    <?php require_once('./header.php') ?>
+
+    <!-- start of container  -->
+    <div class="container mb-6">
+
+        <section class="section is-medium">
+            <h1 class="is-size-2 has-text-weight-bold">Services
+                <sub class="is-size-5 has-text-weight-medium">
+                    <il>we offer</il>
+                </sub>
+            </h1>
+
+        </section>
+
+
+        <div class="SignUp">
+            <img src="undraw/undraw_portfolio_feedback_6r17.svg" alt="Undraw">
+            <h1 class="is-size-3 has-text-weight-bold">Sign Up</h1>
+            <p class="is-size-6 has-text-weight-medium">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto architecto iure in ipsa impedit quo porro, fugit eveniet at ullam ex accusantium consectetur? Accusantium officia id amet ex quia expedita.</p>
+
+        </div>
+        <section class="section is-medium">
+            <div class="SignIn">
+                <img src="undraw/undraw_social_bio_re_0t9u (1).svg" alt="Undraw">
+                <h1 class="is-size-3 has-text-weight-bold">Sign In</h1>
+                <p class="is-size-6 has-text-weight-medium">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae inventore, adipisci aperiam blanditiis perferendis impedit velit a cupiditate libero, maiores possimus ipsum placeat minima magni voluptatibus assumenda vitae accusantium architecto.</p>
+            </div>
+        </section>
+
+        <section class="section is-medium">
+            <div class="FreeCourses">
+                <img src="undraw/undraw_youtube_tutorial_re_69qc (1).svg" alt="Undraw">
+                <h1 class="is-size-3 has-text-weight-bold">Free Courses</h1>
+                <p class="is-size-6 has-text-weight-medium">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae inventore, adipisci aperiam blanditiis perferendis impedit velit a cupiditate libero, maiores possimus ipsum placeat minima magni voluptatibus assumenda vitae accusantium architecto.</p>
+            </div>
+        </section>
+
+        <section class="section is-medium">
+        <div class="PaidCourses">
+                <img src="undraw/undraw_online_media_re_r9qv.svg" alt="Undraw">
+                <h1 class="is-size-3 has-text-weight-bold">Paid Courses</h1>
+                <p class="is-size-6 has-text-weight-medium">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae inventore, adipisci aperiam blanditiis perferendis impedit velit a cupiditate libero, maiores possimus ipsum placeat minima magni voluptatibus assumenda vitae accusantium architecto.</p>
+            </div>
+        </section>
+<br>
+        <section class="section is-medium">
+
+        </section>
+        <section class="section is-medium">
+
+</section>
+<br/><br/><br/><br/><br/>
+
+<!-- Finished -->
+
+
+    </div>
+    <!-- end of container -->
+
+
+
+    <?php require_once('./footer.php') ?>
+    <!-- javascript file -->
+    <script src="./resources/js/index.js"></script>
+</body>
+
+</html>
