edb79df7fceb568f45fd0e6029f0f60810b0fa90
myot0253@gmail.com <myot0253@gmail.com>
2022-07-21 09:57:58 -1200

    Change

--- 04.Development/Level_Up/Student/View/Service.php
+++ 04.Development/Level_Up/Student/View/Service.php
@@ -8,70 +8,56 @@
     <title>Level Up</title>
     <link rel="stylesheet" href="./resources/css/mystyles.css">
     <link rel="stylesheet" href="./resources/css/root.css">
-    <link rel="stylesheet" href="Service.css">
-</head>
+    <link rel="stylesheet" href="service.css">
+</head>     
 
 <body>
     <?php require_once('./header.php') ?>
-
     <!-- start of container  -->
     <div class="container mb-6">
-
         <section class="section is-medium">
             <h1 class="is-size-2 has-text-weight-bold">Services
                 <sub class="is-size-5 has-text-weight-medium">
                     <il>we offer</il>
                 </sub>
             </h1>
-
         </section>
-
-
         <div class="SignUp">
             <img src="undraw/undraw_portfolio_feedback_6r17.svg" alt="Undraw">
             <h1 class="is-size-3 has-text-weight-bold">Sign Up</h1>
             <p class="is-size-6 has-text-weight-medium">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto architecto iure in ipsa impedit quo porro, fugit eveniet at ullam ex accusantium consectetur? Accusantium officia id amet ex quia expedita.</p>
-
+                dolor sit amet consectetur adipisicing elit. Iusto architecto iure in ipsa impedit quo porro, fugit eveniet at ullam ex accusantium consectetur? Accusantium officia id amet ex quia expedita.</p>
         </div>
         <section class="section is-medium">
             <div class="SignIn">
                 <img src="undraw/undraw_social_bio_re_0t9u (1).svg" alt="Undraw">
                 <h1 class="is-size-3 has-text-weight-bold">Sign In</h1>
                 <p class="is-size-6 has-text-weight-medium">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae inventore, adipisci aperiam blanditiis perferendis impedit velit a cupiditate libero, maiores possimus ipsum placeat minima magni voluptatibus assumenda vitae accusantium architecto.</p>
+dte libero, maioreinima magni voluptatibus assumenda vitae accusantium architecto.</p>
             </div>
         </section>
-
         <section class="section is-medium">
             <div class="FreeCourses">
                 <img src="undraw/undraw_youtube_tutorial_re_69qc (1).svg" alt="Undraw">
                 <h1 class="is-size-3 has-text-weight-bold">Free Courses</h1>
                 <p class="is-size-6 has-text-weight-medium">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae inventore, adipisci aperiam blanditiis perferendis impedit velit a cupiditate libero, maiores possimus ipsum placeat minima magni voluptatibus assumenda vitae accusantium architecto.</p>
+                    e velit a cupiro, maissimus ipsum placeat minima magni voluptatibus assumenda vitae accusantium architecto.</p>
             </div>
         </section>
-
         <section class="section is-medium">
-        <div class="PaidCourses">
+            <div class="PaidCourses">
                 <img src="undraw/undraw_online_media_re_r9qv.svg" alt="Undraw">
                 <h1 class="is-size-3 has-text-weight-bold">Paid Courses</h1>
                 <p class="is-size-6 has-text-weight-medium">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae inventore, adipisci aperiam blanditiis perferendis impedit velit a cupiditate libero, maiores possimus ipsum placeat minima magni voluptatibus assumenda vitae accusantium architecto.</p>
+                    Lorem iponing elitventore, adipisci aperiam blanditiis perferendis impedit velit a cupiditate libero, maiores possimus ipsum placeat minima magni voluptatibus assumenda vitae accusantium architecto.</p>
             </div>
         </section>
-<br>
         <section class="section is-medium">
 
         </section>
         <section class="section is-medium">
-
-</section>
-<br/><br/><br/><br/><br/>
-
-<!-- Finished -->
-
-
+        </section>
+        <br /><br /><br /><br /><br />
     </div>
     <!-- end of container -->
 
