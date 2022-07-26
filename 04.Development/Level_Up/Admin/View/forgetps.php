19085ed2d7e480a99afadf5465d2d60ef0ab363a
myot0253@gmail.com <myot0253@gmail.com>
2022-07-26 01:45:57 -1200

    upgrade

--- 04.Development/Level_Up/Admin/View/forgetps.php
+++ 04.Development/Level_Up/Admin/View/forgetps.php
@@ -1,40 +1,58 @@
-<?php
-$time = time();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Level Up</title>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <link rel="stylesheet" href="./resources/css/index.css?<?php echo $time ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Level up - Sign In</title>
+    <link rel="icon" type="image/x-icon" href="./resources/img/common/favicon.png">
+    <link rel="stylesheet" href="./resources/css/mystyles.css">
+    <link rel="stylesheet" href="./resources/css/auth.css">
+    <link rel="stylesheet" href="forgetps.css">
 </head>
 
 <body>
-
-    <?php require_once('./sidebar.php') ?>
-
-    <section class="container">
+    <section class="is-medium">
+        <div class="columns is-gapless is-desktop">
+            <div class="column column-fill signin-left-section">
+                <div class="column p-5">
+                    <img width="100px" src="image/Header.svg" alt="Header">
+                </div>
+                <div class="column mr-10 ml-10 mt-4">
+                    <p id="p_title" class="is-size-2 has-text-weight-bold m-auto">Forget Password</p>
+                    <div class="field mt-6">
+                        <div class="control">
+                            <p id="p_title" class="is-size-6 has-text-weight-semibold m-auto">Enter the email address you used in registration and we will send you instructions to reset your password.Enter the email address you used in registration and we will send you instructions to reset your password.</p>
+                        </div>
+                    </div>
+                    <div class="field mt-5">
+                        <div class="control">
+                            <p id="p_title" class="is-size-6 has-text-weight-semibold m-auto">Enter the email address you used in registration and we will send you instructions to reset your password.Enter the email address you used in registration and we will send you instructions to reset your password.Enter the email address you used in registration and we will send you instructions to reset your password.</p>
+                        </div>
+                    </div>
+                    <div class="field mt-5">
+                        <div class="control">
+                            <p id="p_title" class="is-size-6 has-text-weight-semibold m-auto">Enter the email address you used in registration and we will send you instructions to reset your password.Enter the email address you used in registration and we will send you instructions to reset your password.Enter the email address you used in registration and we will send you instructions to reset your password.Enter the email address you used in registration and we will send you instructions to reset your password.</p>
+                        </div>
+                    </div>
+                    <div class="field mt-6">
+                        <label class="label is-primay-label has-text-weight-semibold ">Email Address</label>
+                        <div class="control">
+                            <input id="email" class="input text-box" type="email" placeholder="Eg.John123@gmail.com">
+                        </div>
+                    </div>
+                    <div class="column is-full">
+                        <button class=" button is-warning is-size-6 has-text-weight-bold ">Send Email Link</button>
+                    </div>
+                </div>
+            </div>
+            <div class="column column-fill is-hidden-mobile is-hidden-tablet-only sign-in-graphics">
+                <img class="forget_arc" src="image/Group 33915 (2).png" alt="">
+            </div>
+        </div>
     </section>
-
-    <script>
-        const body = document.querySelector('body'),
-            sidebar = body.querySelector('nav'),
-            toggle = body.querySelector(".toggle"),
-            searchBtn = body.querySelector(".search-box"),
-            modeSwitch = body.querySelector(".toggle-switch"),
-            modeText = body.querySelector(".mode-text");
-
-
-        toggle.addEventListener("click", () => {
-            sidebar.classList.toggle("close");
-        })
-    </script>
 </body>
+
 
 </html>
