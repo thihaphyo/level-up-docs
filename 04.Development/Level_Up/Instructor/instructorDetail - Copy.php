0b804bd2b591bd806cb2d3995a3a6f5c6d107007
ba_c <swunhtetoo@gmail.com>
2022-07-26 10:24:05 +0630

    initial

--- 04.Development/Level_Up/Instructor/instructorDetail - Copy.php
+++ 04.Development/Level_Up/Instructor/instructorDetail - Copy.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Level Up</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-    <link rel="stylesheet" href="../View/resources/css/instructorList.css">
+    <link rel="stylesheet" href="./instructorList.css">
 </head>
 
 <body>
