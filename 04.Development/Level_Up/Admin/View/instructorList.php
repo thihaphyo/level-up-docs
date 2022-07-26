381873be2f943f8bb0ded51972211e07f3d6ec0b
ZMHtet <zmhtet.200111@gmail.com>
2022-07-26 18:25:06 +0630

    Admin Instructor List

---
+++ 04.Development/Level_Up/Admin/View/instructorList.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Level Up</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="stylesheet" href="../View/resources/css/instructorList.css?v=<?time()?>">
+</head>
+
+<body>
+    <main>
+        <div class="sideNavbar"></div>
+        <!-- start of container  -->
+        <div class="container">
+            <h2 class="title is-3">Instructors List<a href="./instructorDetail.php" class="view">View Instructor Requests</a></h2>
+            <div class="instructors">
+
+                <div class="instructor">
+                    <div class="profile">
+                        <img src="../View/resources/img/Group 33668.svg" alt="">
+                        <p class="detail"><a href="#">more detail</a></p>
+                    </div>
+                    <div class="instructorName">
+                        <div class="name">Name
+                            <h2 class="title is-6">Michael John</h2>
+                        </div>
+                        <div class="phone">Phone
+                            <h2 class="title is-6">+95 9358395039</h2>
+                        </div>
+                    </div>
+                    <div class="exprience">Exprience
+                        <h2 class="title is-6">Web Developer</h2>
+                    </div>
+                    <div class="address">
+                        <div class="road">Address
+                            <h2 class="title is-6">St. Taleat Tuabl</h2>
+                        </div>
+                        <div class="email">Email
+                            <h2 class="title is-6">michale3049@gmail.com</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="instructor">
+                    <div class="profile">
+                        <img src="../View/resources/img/Group 33668.svg" alt="">
+                        <p class="detail"><a href="#">more detail</a></p>
+                    </div>
+                    <div class="instructorName">
+                        <div class="name">Name
+                            <h2 class="title is-6">Michael John</h2>
+                        </div>
+                        <div class="phone">Phone
+                            <h2 class="title is-6">+95 9358395039</h2>
+                        </div>
+                    </div>
+                    <div class="exprience">Exprience
+                        <p class="title is-6">Web Developer</p>
+                    </div>
+                    <div class="address">
+                        <div class="road">Address
+                            <h2 class="title is-6">St. Taleat Tuabl</h2>
+                        </div>
+                        <div class="email">Email
+                            <h2 class="title is-6">michale3049@gmail.com</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="instructor">
+                    <div class="profile">
+                        <img src="../View/resources/img/Group 33668.svg" alt="">
+                        <p class="detail"><a href="#">more detail</a></p>
+                    </div>
+                    <div class="instructorName">
+                        <div class="name">Name
+                            <h2 class="title is-6">Michael John</h2>
+                        </div>
+                        <div class="phone">Phone
+                            <h2 class="title is-6">+95 9358395039</h2>
+                        </div>
+                    </div>
+                    <div class="exprience">Exprience
+                        <p class="title is-6">Web Developer</p>
+                    </div>
+                    <div class="address">
+                        <div class="road">Address
+                            <h2 class="title is-6">St. Taleat Tuabl</h2>
+                        </div>
+                        <div class="email">Email
+                            <h2 class="title is-6">michale3049@gmail.com</h2>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="paginationBar">
+                <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+                    <a class="pagination-previous">Previous</a>
+                    <a class="pagination-next">Next page</a>
+                    <ul class="pagination-list">
+                        <li><a class="pagination-link" aria-label="Goto page 1">1</a></li>
+                        <li><span class="pagination-ellipsis">&hellip;</span></li>
+                        <li><a class="pagination-link" aria-label="Goto page 45">45</a></li>
+                        <li><a class="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a></li>
+                        <li><a class="pagination-link" aria-label="Goto page 47">47</a></li>
+                        <li><span class="pagination-ellipsis">&hellip;</span></li>
+                        <li><a class="pagination-link" aria-label="Goto page 86">86</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+        <!-- end of container -->
+    </main>
+    <!-- javascript file -->
+    <script src="../View/resources/js/adAbout.js"></script>
+</body>
+
+</html>
