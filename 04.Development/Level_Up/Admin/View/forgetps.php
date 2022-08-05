321f595d5a41f5beae1a04ca5da3b41f90ca4412
myot0253@gmail.com <myot0253@gmail.com>
2022-08-04 18:29:20 -1200

    updated

--- 04.Development/Level_Up/Admin/View/forgetps.php
+++ 04.Development/Level_Up/Admin/View/forgetps.php
@@ -23,21 +23,21 @@
                     <p id="p_title" class="is-size-2 has-text-weight-bold m-auto">Forget Password</p>
                     <div class="field mt-6">
                         <label class="label is-primay-label has-text-weight-semibold ">For security reasons, we do NOT store your passwords, be assured that we will never send your password via email.For security reasons, we do NOT store your passwords, be assured that we will never send your password via email.</label>
-                       
                     </div>
-                    <div class="field mt-4 ">
-                        <label class="label is-primay-label has-text-weight-semibold ">For security reasons, we do NOT store your passwords, be assured that we will never send your password via email. For security reasons, we do NOT store your passwords, be assured that we will never send your password via email.</label>
-                        
+                    <div class="field mt-5 ">
+                        <label class="label is-primay-label has-text-weight-semibold "> For security reasons, we do NOT store your passwords, be assured that we will never send your password via email.</label>
                     </div>
-                    <div class="field mt-6">
-                        <label class="label is-primay-label has-text-weight-semibold ">Email Address</label>
-                        <div class="control">
-                            <input id="email" class="input text-box is-black" type="email" placeholder="Eg.John123@gmail.com">
+                    <form action="sendmail.php" method="POST">
+                        <div class="field mt-6">
+                            <label class="label is-primay-label has-text-weight-semibold ">Email Address</label>
+                            <div class="control">
+                                <input id="email" class="input text-box is-black" type="email" placeholder="Eg.John123@gmail.com">
+                            </div>
                         </div>
-                    </div>
-                    <div class="column is-full">
-                        <button class=" button is-warning is-size-6 has-text-weight-bold ">Send Email Link</button>
-                    </div>
+                        <div class="buttonchange column is-full">
+                            <button class=" button is-warning   is-size-6 has-text-weight-bold ">Send Email Link</button>
+                        </div>
+                    </form>
                 </div>
             </div>
             <div class="column column-fill is-hidden-mobile is-hidden-tablet-only sign-in-graphics">
