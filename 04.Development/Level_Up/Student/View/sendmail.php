fc247a2e2bd75baf536014cb8416505c05eeb0d1
myot0253@gmail.com <myot0253@gmail.com>
2022-08-04 18:08:13 -1200

    upgrade

--- 04.Development/Level_Up/Student/View/sendmail.php
+++ 04.Development/Level_Up/Student/View/sendmail.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Send mail</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+
+
+
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
@@ -65,3 +79,6 @@ try {
 } catch (Exception $e) {
     echo "Mailer Error: " . $mail->ErrorInfo;
 }
+?>
+</body>
+</html>
